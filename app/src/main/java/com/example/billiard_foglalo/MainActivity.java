e5acce77654f4c468304408914aa90d8f22b46d1
kovacsmate17 <h266626@stud.u-szeged.hu>
2025-04-13 19:05:37 +0200

    simple animation on login page

--- app/src/main/java/com/example/billiard_foglalo/MainActivity.java
+++ app/src/main/java/com/example/billiard_foglalo/MainActivity.java
@@ -4,21 +4,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getName();
     private EditText emailTextInput, passwordTextInput;
-    private Button loginBTN;
     private FirebaseAuth auth;
+
+    ImageView logo;
+    TextInputLayout emailLayout, passwordLayout;
+    Button loginButton, registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +34,22 @@ public class MainActivity extends AppCompatActivity {
 
         emailTextInput = findViewById(R.id.emailTextInput);
         passwordTextInput = findViewById(R.id.passwordTextInput);
-        loginBTN = findViewById(R.id.loginButton);
 
-        loginBTN.setOnClickListener(this::loginWithEmail);
+        logo = findViewById(R.id.logo);
+        emailLayout = findViewById(R.id.emailLayout);
+        passwordLayout = findViewById(R.id.passwordLayout);
+        loginButton = findViewById(R.id.loginButton);
+        registerButton = findViewById(R.id.registerButton);
+        loginButton.setOnClickListener(this::loginWithEmail);
 
         auth = FirebaseAuth.getInstance();
+
+        Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+
+        new android.os.Handler().postDelayed(() -> emailLayout.startAnimation(fadeIn), 100);
+        new android.os.Handler().postDelayed(() -> passwordLayout.startAnimation(fadeIn), 200);
+        new android.os.Handler().postDelayed(() -> loginButton.startAnimation(fadeIn), 300);
+        new android.os.Handler().postDelayed(() -> registerButton.startAnimation(fadeIn), 400);
     }
 
     public void loginWithEmail(View view) {
@@ -57,20 +75,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void loginWithGoogle(View view){
 
-    }
-
-    public void loginAsGuest(View view){
-        auth.signInAnonymously().addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()){
-                    Log.d(LOG_TAG, "Sikeres bejelentkezés vendég");
-                    startBookingActivity();
-                }else{
-                    Log.d(LOG_TAG, "Sikertelen bejelentkezés vendég");
-                }
-            }
-        });
     }
 
     public void redirectToRegister(View view) {
